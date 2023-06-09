1ead4231d67fae1637e56529620e2d6047ed4be2
b3ny4 <b3ny4@protonmail.com>
2023-06-09 21:52:18 +0200

    crud

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 
 // import database models
 import Gym from './models/Gym.js';
@@ -14,6 +15,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.use(bodyParser.json())
 
 mongoose.connect('mongodb://localhost:27017/yelpgym');
 const db = mongoose.connection;
@@ -30,7 +32,28 @@ app.get("/", (req, res) => {
 app.get("/gyms", async (req, res) => {
     const gyms = await Gym.find({});
     res.send({ gyms });
-})
+});
+
+app.post("/gyms", async (req, res) => {
+    const gym = new Gym(req.body);
+    await gym.save();
+    res.send(gym._id);
+});
+
+app.get("/gyms/:id", async (req, res) => {
+    const gym = await Gym.findById(req.params.id);
+    res.send(gym);
+});
+
+app.put("/gyms/:id", async (req, res) => {
+    const gym = await Gym.findByIdAndUpdate(req.params.id, req.body);
+    res.send(gym._id);
+});
+
+app.delete("/gyms/:id", async (req, res) => {
+    const gym = await Gym.findByIdAndDelete(req.params.id);
+    res.send("ok");
+});
 
 app.listen(3001, () => {
     console.log('listening on port 3001 ...');
